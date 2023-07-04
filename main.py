14ba1aa8e7276d8e99a88f3eddbb94d44aefadfb
yanbalx <138567905+yanbalx@users.noreply.github.com>
2023-07-04 16:29:36 +0800

    Update main.py

--- main.py
+++ main.py
@@ -21,8 +21,8 @@ template_id = os.environ["TEMPLATE_ID"]
 def get_weather():
   url = "http://autodev.openspeech.cn/csp/api/v2.1/weather?openId=aiuicus&clientType=android&sign=android&city=" + city
   res = requests.get(url).json()
-  weather = res['data']['list'][0]
-  return weather['weather'], math.floor(weather['temp'])
+  weather = res['wea']
+  return weather['weather'], math.floor(res['tem'])
 
 def get_count():
   delta = today - datetime.strptime(start_date, "%Y-%m-%d")
