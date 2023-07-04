ee3a5b213884d8c7be50c6697d7836cf0695a02d
yanbalx <138567905+yanbalx@users.noreply.github.com>
2023-07-04 16:50:58 +0800

    Update main.py

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ template_id = os.environ["TEMPLATE_ID"]
 
 def get_weather():
   url = "https://v0.yiketianqi.com/api?unescape=1&version=v61&appid=97897565&appsecret=zUECL8HV&ext=&cityid=&city=厦门"
-  res = requests.get(url)
+  res = requests.get(url).json()
   return res['wea'], math.floor(res['tem'])
 
 def get_count():
